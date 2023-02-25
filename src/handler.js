3019f26dab7c43bf19d3dc17c29410305b836b3c
pramanarendra <albertushandypramana@gmail.com>
2023-02-25 18:37:11 +0700

    add get book detail by id

--- src/handler.js
+++ src/handler.js
@@ -76,4 +76,26 @@ const getAllBooksHandler = () => ({
   }
 })
 
-module.exports = { addBooksHandler, getAllBooksHandler };
+const getBooksByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const book = books.filter((book) => book.id === id)[0];
+
+  if(book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    }
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan'
+  })
+  response.code(404);
+  return response;
+}
+
+module.exports = { addBooksHandler, getAllBooksHandler, getBooksByIdHandler };
